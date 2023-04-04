32d20e6881369a6ddc29c6266b9b56ae0741f499
Lyonel Pierce <lyonel@live.com>
2023-04-04 14:03:40 -0400

    Demo excersises

---
+++ 1_week/tue/demo/demo.js
@@ -0,0 +1,57 @@
+// console.log('Hello World');
+
+// const firstName = "Lyonel";
+// let age = 40;
+
+// console.log(`FirstName: ${firstName}, Age: ${age}`);
+
+// const isCool = true;
+
+// if(isCool === true){
+//     console.log(`${firstName} is cool`);
+// } else {
+//     console.log(`${firstName} is not cool`);
+// }
+
+// let x = 4;
+// let y = 10;
+// let z = 7 + 8;
+// let sum = x+y+z;
+// console.log(sum);
+// console.log(sum + y);
+
+// console.log(sum  - z  + sum + y);
+
+// let str = "String";
+// let str1 = 'String';
+// let str3 = `String ${str} ${str1}`;
+// let quote = "He said to her, 'Hello there'. Continue";
+// console.log(str3);
+
+// let str4 = "String " + "String " + str3;
+// console.log(str4);
+
+let away = 115;
+let home = 115;
+
+if(away > home){
+    console.log('Away team wins!!');
+} else if( home > away) {
+    console.log('Home team wins!!');
+} else {
+    console.log('Tie');
+}
+
+let age = 10
+let age1 = 20
+let age2 = 21
+
+if(age >= age1){
+    console.log(age);
+}
+if(age === age2){
+    console.log('It is the same');
+}
+if(age !== age1){
+    console.log('It is not the same!');
+}
