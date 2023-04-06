5579aa8114d67f69f554bc1591d3f85520984c5d
Lyonel Pierce <lyonel@live.com>
2023-04-06 13:07:10 -0400

    Code Challenge

--- 1_week/thu/code_challenge/findTheRange.js
+++ 1_week/thu/code_challenge/findTheRange.js
@@ -1,13 +1,22 @@
-let smallest = array[0], largest = array[0];
+// const array = [-11, 55, 11, 2, 37, -4];
+// let smallest = array[0], largest = array[0];
 
-for (let i = 1; i <= array.length; i++) {
-    if(array[i] < smallest) {
-      smallest = array[i];
-    }
-    if(array[i] > largest)  {
-      largest = array[i];
-    }
+// for (let i = 1; i <= array.length; i++) {
+//     if(array[i] < smallest) {
+//       smallest = array[i];
+//     }
+//     if(array[i] > largest)  {
+//       largest = array[i];
+//     }
+// }
+
+// console.log(`Smallest: ${smallest}`);
+// console.log(`Largest: ${largest}`);
+
+function findRange(numbers){
+  let max = Math.max(...numbers);
+  let min = Math.min(...numbers);
+  return {max, min};
 }
 
-console.log(`Smallest: ${smallest}`);
-console.log(`Largest: ${largest}`);
+console.log(findRange([-11, 55, 11, 2, 37, -4]));
