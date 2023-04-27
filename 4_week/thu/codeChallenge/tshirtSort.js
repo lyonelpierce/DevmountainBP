c2813d8d98a2a0a8e33b7398fa700132833d8563
Lyonel Pierce <lyonel@live.com>
2023-04-27 10:29:15 -0400

    T-Shirt code challenge

--- 4_week/thu/codeChallenge/tshirtSort.js
+++ 4_week/thu/codeChallenge/tshirtSort.js
@@ -1,17 +1,28 @@
+// Function T-Shirt Sorter
 function tshirtSorter(str) {
+  // Initialize counters
   let sCount = 0,
     mCount = 0,
     lCount = 0;
+  // Iterate over each character
   for (let i = 0; i < str.length; i++) {
+    // If character is s increase s count
     if (str[i] === "s") {
       sCount++;
-    } else if (str[i] === "m") {
+    }
+    // If character is m increase m count
+    else if (str[i] === "m") {
       mCount++;
-    } else if (str[i] === "l") {
+    }
+    // If character is l increase l count
+    else if (str[i] === "l") {
       lCount++;
     }
   }
+  // Return s repeated sCount times + m repeated mCount...
   return "s".repeat(sCount) + "m".repeat(mCount) + "l".repeat(lCount);
 }
 
 console.log(tshirtSorter("lllllmmmsmmssss"));
+console.log(tshirtSorter("lllsssmmmssll"));
+console.log(tshirtSorter("mmlmmslsmlls"));
