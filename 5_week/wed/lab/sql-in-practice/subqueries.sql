de932de48152750018f14f194fe32b08bd1810c5
Lyonel Pierce <lyonel@live.com>
2023-05-03 16:30:27 -0400

    Intermediate

---
+++ 5_week/wed/lab/sql-in-practice/subqueries.sql
@@ -0,0 +1,63 @@
+// 30
+
+SELECT * 
+FROM invoice
+WHERE invoice_id IN (
+  SELECT invoice_id
+  FROM invoice_line
+  WHERE unit_price > 0.99
+);
+
+// 6580 
+
+SELECT * 
+FROM playlist_track
+WHERE playlist_id IN (
+  SELECT playlist_id
+  FROM playlist
+  WHERE name = 'Music'
+);
+
+// 1477
+
+SELECT name
+FROM track
+WHERE track_id IN (
+    SELECT track_id
+    FROM playlist_track
+    WHERE playlist_id = 5
+);
+
+// 17 
+
+SELECT *
+FROM track
+WHERE genre_id IN (
+    SELECT genre_id
+    FROM genre
+    WHERE name = 'Comedy'
+);
+
+// 7
+
+SELECT *
+FROM track
+WHERE album_id IN (
+    SELECT album_id
+    FROM album
+    WHERE title = 'Fireball'
+);
+
+// 45
+
+SELECT *
+FROM track
+WHERE album_id IN (
+    SELECT album_id
+    FROM album
+    WHERE artist_id IN (
+        SELECT artist_id
+        FROM artist
+        WHERE name = 'Queen'
+    )
+);
